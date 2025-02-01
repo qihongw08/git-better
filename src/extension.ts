63efacf398ab141755d394b50cb7352bb56a8a08
vivianzo <67518516+vivianzo@users.noreply.github.com>
2025-02-01 14:39:02 -0500

    added a new branch

--- src/extension.ts
+++ src/extension.ts
@@ -17,6 +17,8 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from git-better!');
+
+		vscode.env.openExternal(vscode.Uri.parse('https://github.com/qihongw08/git-better'));
 	});
 
 	context.subscriptions.push(disposable);
