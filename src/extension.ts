76da5fcae018c8f73721cc1d1100ad57c3469775
Vivian Zou <67518516+vivianzo@users.noreply.github.com>
2025-02-01 14:58:21 -0500

    Revert "basic api scaffolding"

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { startServer } from './server';
-
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	startServer();
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -21,13 +17,9 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from git-better!');
-
-		vscode.env.openExternal(vscode.Uri.parse('https://google.com/'));
 	});
 
 	context.subscriptions.push(disposable);
-
-	
 }
 
 // This method is called when your extension is deactivated
