f31d4fbf6a9f8e551944c405254c9a7242e86055
sanjana-singhania <singhania.sa@northeastern.edu>
2025-02-01 15:09:28 -0500

    switched order

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { startServer } from './server';
+import { startServer } from './server/index';
 
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	startServer();
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "git-better" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('git-better.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from git-better!');
 
 		vscode.env.openExternal(vscode.Uri.parse('https://google.com/'));
 	});
-
 	context.subscriptions.push(disposable);
 
-	
+	try {
+		startServer();
+	  } catch (error) {
+		console.error("Error starting the server:", error);
+	}
+
 }
 
 // This method is called when your extension is deactivated
