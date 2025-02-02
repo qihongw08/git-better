5ac3f5cd08a0f336a804cdfc0a102edc974e5792
QW66666 <wusteven0829@gmail.com>
2025-02-01 19:50:54 -0500

    feat: endpoint to get repo comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { GitBetterProvider } from './GitBetterProvider';
-import { startServer } from './server/index';
+import * as vscode from "vscode";
+import { GitBetterProvider } from "./GitBetterProvider";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "git-better" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "git-better.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from git-better!");
+    }
+  );
+  context.subscriptions.push(disposable);
 
-	console.log('Congratulations, your extension "git-better" is now active!');
-	const disposable = vscode.commands.registerCommand('git-better.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from git-better!');
+  const gitBetterProvider = new GitBetterProvider();
+  vscode.window.registerTreeDataProvider("gitBetter", gitBetterProvider);
 
-	});
-	context.subscriptions.push(disposable);
-
-	const gitBetterProvider = new GitBetterProvider();
-	vscode.window.registerTreeDataProvider('gitBetter', gitBetterProvider);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('git-better.refresh', () => gitBetterProvider.refresh())
-	);
-	try {
-		startServer();
-	  } catch (error) {
-		console.error("Error starting the server:", error);
-	}
+  context.subscriptions.push(
+    vscode.commands.registerCommand("git-better.refresh", () =>
+      gitBetterProvider.refresh()
+    )
+  );
+  try {
+    //startServer();
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
 }
 
 // This method is called when your extension is deactivated
